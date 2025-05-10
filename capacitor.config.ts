75afa6e7511bde1dcc5b2251f545b9377f14ec80
John Doe <johndoe@email.com>
2025-05-10 21:28:07 +0300

    Initial commit with Svelte and Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cloud.flowline.app',
+  appName: 'flowline',
+  webDir: 'dist'
+};
+
+export default config;
